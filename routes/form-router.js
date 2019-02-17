56efd3f59644d13bb3a64d779bbd3d68237a195f
Yvens 16 <yvensbelaston@gmail.com>
2019-02-17 21:38:09 +0100

    added one field for the new form

--- routes/form-router.js
+++ routes/form-router.js
@@ -6,10 +6,12 @@ const nodemailer = require('nodemailer');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post('/form', (req, res, next) => {
+    console.log(req.body, 'req.body');
     const output = `
     <h1>We have a new lead</h1>
     <ul>
-        <li>name: ${req.body.name} </li>
+        <li>Nom: ${req.body.name} </li>
+        <li>Prénom: ${req.body.firstName} </li>
         <li>numéro de téléphone: ${req.body.phone} </li>
         <li>Difficulté dans quels matière: ${req.body.diff}</li>
         <li>Comment l'aider vous? : ${req.body.help}</li>
