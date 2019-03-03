0a19a931955c073b47a78625c018546cbab651b8
Yvens 16 <yvensbelaston@gmail.com>
2019-03-03 22:01:34 +0100

    Added routes for step2 application

--- routes/form-router.js
+++ routes/form-router.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post('/form', (req, res, next) => {
     console.log(req.body, 'req.body');
-    const output = `
+    let output = `
     <h1>We have a new lead</h1>
     <ul>
         <li>Nom: ${req.body.name} </li>
@@ -18,7 +18,7 @@ router.post('/form', (req, res, next) => {
         <li>À rappeller le: ${req.body.date}</li>
     </ul>`;
 
-    const msg = {
+    let msg = {
     to: process.env.MAIL2,
     from: process.env.MAIL2,
     subject: 'New Lead ✔',
@@ -27,7 +27,30 @@ router.post('/form', (req, res, next) => {
     };
     
     sgMail.send(msg).catch(err => console.log(err));
+})
 
+router.post('/formulaire', (req, res, next) => {
+    console.log(req.body, 'req.body');
+     let output = `
+    <h1>We have a new lead</h1>
+    <ul>
+        <li>Nom: ${req.body.name} </li>
+        <li>Prénom: ${req.body.firstName} </li>
+        <li>numéro de téléphone: ${req.body.phone} </li>
+        <li>Les 3 dernières notes ne maths: ${req.body.note1}, ${req.body.note2}, ${req.body.note3}</li>
+        <li>Comment l'aider vous? : ${req.body.description}</li>
+        <li>À rappeller le: ${req.body.date}</li>
+    </ul>`;
+
+    let msg = {
+    to: process.env.MAIL2,
+    from: process.env.MAIL2,
+    subject: 'New Lead ✔',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: output,
+    };
+    
+    sgMail.send(msg).catch(err => console.log(err));
 })
 
 
